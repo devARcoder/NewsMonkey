c9038827dcf6d827dcb8a536789c21fb5a5c3a0f
Abdur Razzaq <abdurrazaq789@gmail.com>
2025-02-15 12:23:17 +0500

    lecture 28 ended

--- src/components/News.js
+++ src/components/News.js
@@ -8,40 +8,49 @@ export class News extends Component {
     this.state = {
       articles: [],
       loading: false,
-      page: 1
+      page: 1,
     };
   }
-  async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0a9584b34eab4cb3929218488e65af44&page=1&pageSize=3"
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0a9584b34eab4cb3929218488e65af44&page=1&pageSize=12";
     let data = await fetch(url);
     let parsedData = await data.json();
-    console.log(parsedData)
+    console.log(parsedData);
     this.setState({
-      articles: parsedData.articles})
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
   }
-  handlePreClick = async() =>{
-    console.log("Previvous")
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0a9584b34eab4cb3929218488e65af44&page=${this.state.page - 1}&pageSize=3`
+  handlePreClick = async () => {
+    console.log("Previvous");
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0a9584b34eab4cb3929218488e65af44&page=${
+      this.state.page - 1
+    }&pageSize=12`;
     let data = await fetch(url);
     let parsedData = await data.json();
-    console.log(parsedData)
+    console.log(parsedData);
     this.setState({
       page: this.state.page - 1,
-      articles: parsedData.articles
-    })
-  }
-  handleNextClick = async() =>{
-
-    console.log("Next")
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0a9584b34eab4cb3929218488e65af44&page=${this.state.page + 1}&pageSize=3`;
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    console.log(parsedData)
-    this.setState({
-      page: this.state.page + 1,
-      articles: parsedData.articles
-    })
-  }
+      articles: parsedData.articles,
+    });
+  };
+  handleNextClick = async () => {
+    console.log("Next");
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0a9584b34eab4cb3929218488e65af44&page=${
+        this.state.page + 1
+      }&pageSize=12`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
+    }
+  };
   render() {
     return (
       <div>
@@ -49,25 +58,40 @@ export class News extends Component {
           NewsMonkey - Top Headlines
         </h1>
         <div className="grid grid-cols-1 gap-2 justify-center justify-items-center md:grid md:grid-cols-3 md:gap-5 md:justify-center md:justify-items-center">
-
-        {this.state.articles.map((element) => {
-          return (
-            <div className="" key={element.url}>
-              <NewsItems
-              name={element.source.name}
-                title={element.title?element.title.slice(0, 40):""}
-                desc={element.description?element.description.slice(0, 120):""}
-                imageUrl={element.urlToImage}
-                newsUrl={element.url}
-                published={element.publishedAt}
+          {this.state.articles.map((element) => {
+            return (
+              <div className="" key={element.url}>
+                <NewsItems
+                  name={element.source.name}
+                  title={element.title ? element.title.slice(0, 40) : ""}
+                  desc={
+                    element.description ? element.description.slice(0, 120) : ""
+                  }
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                  published={element.publishedAt}
                 />
-            </div>
-          );
-        })}
+              </div>
+            );
+          })}
         </div>
         <div className="buttons flex justify-between items-center mx-8 my-2">
-          <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePreClick}> &larr; Previous</button>
-          <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr; </button>
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePreClick}
+          >
+            {" "}
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;{" "}
+          </button>
         </div>
       </div>
     );
